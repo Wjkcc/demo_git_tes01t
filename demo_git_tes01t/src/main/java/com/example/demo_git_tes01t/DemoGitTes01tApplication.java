c80226fdc076c5f7603ed398dbc7aff1968b7d46
Joe <1527008697@qq.com>
2020-03-25 13:54:47 +0800

    sss

---
+++ demo_git_tes01t/src/main/java/com/example/demo_git_tes01t/DemoGitTes01tApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_git_tes01t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitTes01tApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitTes01tApplication.class, args);
+    }
+
+}
---
+++ demo_git_tes01t/src/test/java/com/example/demo_git_tes01t/DemoGitTes01tApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_git_tes01t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitTes01tApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
